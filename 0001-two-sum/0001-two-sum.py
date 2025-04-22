96500981f5c9e39885cf50d2e5bcfa8d23accb8a
Prince <107897425+prince950408@users.noreply.github.com>
2025-04-22 07:00:57 -0400

    Time: 0 ms (100%), Space: 13.1 MB (61.8%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pair_idx = {}
+
+        for i, num in enumerate(nums):
+            if target - num in pair_idx:
+                return [i, pair_idx[target - num]]
+            pair_idx[num] = i
